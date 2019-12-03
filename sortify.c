f410ee4f9294fede11be69458c7951045998ec8c
Pedro Marques <pmark.pt@gmail.com>
2019-12-03 22:25:42 +0000

    made size of array configurable through an argument; game state logic

--- sortify.c
+++ sortify.c
@@ -14,12 +14,14 @@
 #define MSG_MAX "You have reached the maximum number of moves."
 #define MSG_BYE "Bye."
 #define MSG_UNKNOWN "Unknown option."
+#define MAX_ROUNDS 30
 /* Use puts() to print constant strings */
 
 //// EXTRA
 #define N_SIZE 4 // Affects the number of numbers that the player has to order. Default is 4.
 
-void play(unsigned short int *, unsigned short int *, unsigned short int *, unsigned short int *, short int *, short int *);
+void play(unsigned short int, unsigned short int *, unsigned short int *, unsigned short int *, unsigned short int *, short int *, short int *);
+int clean_input_buffer();
 int rand_number(const int, const int);
 void print_status(const int, const int, const int);
 void print_menu(void);
@@ -27,37 +29,30 @@ void print_menu(void);
 int main(int argc, char **argv)
 {
 	char user_input;
-	unsigned short int input_count, level = 0, current_round = 0, current_points = 0, level_threshold[5] = {10, 20, 30, 40, 50};
+	unsigned short int n_size = N_SIZE, level = 0, current_round = 0, current_points = 0, level_threshold[5] = {10, 20, 30, 40, 50};
 	short int min_values[5] = {0, 0, -50, -100, -200}, max_values[5] = {10, 30, 30, 0, -100};
 	bool active = true;
 
 	puts(MSG_WELCOME);
 
 	// Handle Arguments
-	if (argc == 1)
-		srand(time(NULL));
-	else
+	if (argc == 2)
 		srand(atoi(argv[1]));
+	else if (argc == 3 && atoi(argv[2]) > 3)
+		n_size = atoi(argv[2]);
+	else
+		srand(time(NULL));
 
 	print_menu();
 	while (active)
 	{
-		input_count = 0;
-
-		scanf("%c", &user_input);
+		scanf(" %c", &user_input);
 
 		if (user_input == '\n')
 			continue;
 
-		// Check for input buffer, count size
-		int c;
-		while ((c = getchar()) != '\n' && c != EOF)
-		{
-			input_count++;
-		}
-
 		// If user fails to input only one char that's an unknown command
-		if (input_count > 0)
+		if (clean_input_buffer() > 0)
 		{
 			puts(MSG_UNKNOWN);
 			continue;
@@ -79,7 +74,7 @@ int main(int argc, char **argv)
 				break;
 
 			case 'p':
-				play(&level, &current_round, &current_points, level_threshold, min_values, max_values);
+				play(n_size, &level, &current_round, &current_points, level_threshold, min_values, max_values);
 				break;
 
 			default:
@@ -90,15 +85,54 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+// Cleans Input Buffer and Returns Size
+int clean_input_buffer()
+{
+	short int c;
+	unsigned short int input_count = 0;
+
+	while ((c = getchar()) != '\n' && c != EOF)
+	{
+		input_count++;
+	}
+
+	return input_count;
+}
+
+/* Handle Player Input
+Returns player's success */
+bool handle_player_input(short int * numbers, short int * player_numbers)
+{
+	unsigned short int i;
+
+	for (i = 0; i < N_SIZE; i++)
+	{
+		scanf("%hi", &player_numbers[i]);
+	}
+
+	clean_input_buffer();
+
+	for (i = 0; i < N_SIZE; i++)
+	{
+		if (*(player_numbers + i) != *(numbers + i))
+		{
+			return false;
+		}
+	}
+
+
+	return true;
+}
+
 // Orders numbers
-void order_numbers(short int * numbers)
+void order_numbers(unsigned short int n_size, short int * numbers)
 {
 	unsigned short int i, j;
 	short int t;
 
-	for (i = 0; i < N_SIZE; i++)
+	for (i = 0; i < n_size; i++)
 	{
-		for (j = 0; j < N_SIZE; j++)
+		for (j = 0; j < n_size; j++)
 		{
 			if (*(numbers + i) < *(numbers + j))
 			{
@@ -112,31 +146,64 @@ void order_numbers(short int * numbers)
 }
 
 // Generated the random numbers and displays them
-void generate_numbers_and_display(unsigned short int * level, short int * min_values, short int * max_values, short int * numbers)
+void generate_numbers_and_display(unsigned short int n_size, unsigned short int * level, short int * min_values, short int * max_values, short int * numbers)
 {
 	puts(MSG_SORT);
-	for (unsigned short i = 0; i < N_SIZE; i++)
+	for (unsigned short i = 0; i < n_size; i++)
 	{
 		*(numbers + i) = rand_number(*(min_values + *level), *(max_values + *level));
 		printf("%d", *(numbers + i));
-		if (i < N_SIZE - 1)
+		if (i < n_size - 1)
 			printf(", ");
 		else
 			printf("\n");
 	}
 }
 
-void play(unsigned short int * level, unsigned short int * current_round,
+void play(unsigned short int n_size, unsigned short int * level, unsigned short int * current_round,
 					unsigned short int * current_points, unsigned short int * level_threshold, short int * min_values, short int * max_values)
 {
-	short int current_generated_numbers[N_SIZE], current_player_numbers[N_SIZE];
+	short int current_generated_numbers[n_size], current_player_numbers[n_size];
+	bool player_success, player_wins = false;
 
 	// + 1 Round
 	*current_round = *current_round + 1;
 
-	generate_numbers_and_display(level, min_values, max_values, current_generated_numbers);
+	generate_numbers_and_display(n_size, level, min_values, max_values, current_generated_numbers);
 
-	order_numbers(current_generated_numbers);
+	order_numbers(n_size, current_generated_numbers);
+
+	player_success = handle_player_input(current_generated_numbers, current_player_numbers);
+	if (player_success)
+	{
+		puts(MSG_WELL);
+		*current_points = *current_points + 5;
+		if (*current_points == level_threshold[*level])
+		{
+			if (*level == 4)
+				player_wins = true;
+			else
+				*level = *level + 1;
+		}
+	}
+	else
+	{
+		puts(MSG_WRONG);
+	}
+
+	if (*current_round == MAX_ROUNDS && !player_wins)
+	{
+		puts(MSG_MAX);
+		print_status(*level + 1, *current_points, *current_round);
+		puts(MSG_OVER);
+	}
+	else if (player_wins)
+	{
+		puts(MSG_WIN);
+		print_status(*level + 1, *current_points, *current_round);
+		puts(MSG_OVER);
+	}
+
 }
 
 /* generate a random integer between min and max */
